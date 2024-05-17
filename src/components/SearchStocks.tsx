31cf804b51a9ee1df9e82ab22303d2d4241876a8
DhirajsGithub <borsedhiraj123@gmail.com>
2024-05-17 13:52:53 +0530

    small changes

--- src/components/SearchStocks.tsx
+++ src/components/SearchStocks.tsx
@@ -43,135 +43,135 @@ const SearchStocks: React.FC<SearchStocksProps> = (props) => {
       });
       console.log(response)
       setLoading(false);
-      let res = [
-        {
-          "1. symbol": "BST",
-          "2. name": "BlackRock Science and Technology Trust",
-          "3. type": "ETF",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "1.0000",
-        },
-        {
-          "1. symbol": "BSTC",
-          "2. name": "Biospecifics Technologies Corp",
-          "3. type": "Equity",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.8571",
-        },
-        {
-          "1. symbol": "BSTG",
-          "2. name": "Biostage",
-          "3. type": "Equity",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.8571",
-        },
-        {
-          "1. symbol": "BST.FRK",
-          "2. name": "Bastei Lübbe AG",
-          "3. type": "Equity",
-          "4. region": "Frankfurt",
-          "5. marketOpen": "08:00",
-          "6. marketClose": "20:00",
-          "7. timezone": "UTC+02",
-          "8. currency": "EUR",
-          "9. matchScore": "0.7500",
-        },
-        {
-          "1. symbol": "BSTAX",
-          "2. name": "BlackRock Total Factor Fund Ivestor A shares",
-          "3. type": "Mutual Fund",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.7500",
-        },
-        {
-          "1. symbol": "BSTBF",
-          "2. name": "Betsson AB",
-          "3. type": "Equity",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.7500",
-        },
-        {
-          "1. symbol": "BSTCX",
-          "2. name": "BlackRock Total Factor Fund Investor C Shares",
-          "3. type": "Mutual Fund",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.7500",
-        },
-        {
-          "1. symbol": "BSTGD",
-          "2. name": "Biostage Inc",
-          "3. type": "Equity",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.7500",
-        },
-        {
-          "1. symbol": "BSTGF",
-          "2. name": "Boustead Singapore Ltd",
-          "3. type": "Equity",
-          "4. region": "United States",
-          "5. marketOpen": "09:30",
-          "6. marketClose": "16:00",
-          "7. timezone": "UTC-04",
-          "8. currency": "USD",
-          "9. matchScore": "0.7500",
-        },
-        {
-          "1. symbol": "BST.DEX",
-          "2. name": "Bastei Lübbe AG",
-          "3. type": "Equity",
-          "4. region": "XETRA",
-          "5. marketOpen": "08:00",
-          "6. marketClose": "20:00",
-          "7. timezone": "UTC+02",
-          "8. currency": "EUR",
-          "9. matchScore": "0.6667",
-        },
-      ];
-      const temp = [];
-      for (let i = 0; i < res.length; i++) {
-        temp.push({
-          name: res[i]["2. name"],
-          symbol: res[i]["1. symbol"],
-        });
-      }
-
-
-
-      // let temp = [];
-      // for (let i = 0; i < response.data.bestMatches.length; i++) {
+      // let res = [
+      //   {
+      //     "1. symbol": "BST",
+      //     "2. name": "BlackRock Science and Technology Trust",
+      //     "3. type": "ETF",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "1.0000",
+      //   },
+      //   {
+      //     "1. symbol": "BSTC",
+      //     "2. name": "Biospecifics Technologies Corp",
+      //     "3. type": "Equity",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.8571",
+      //   },
+      //   {
+      //     "1. symbol": "BSTG",
+      //     "2. name": "Biostage",
+      //     "3. type": "Equity",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.8571",
+      //   },
+      //   {
+      //     "1. symbol": "BST.FRK",
+      //     "2. name": "Bastei Lübbe AG",
+      //     "3. type": "Equity",
+      //     "4. region": "Frankfurt",
+      //     "5. marketOpen": "08:00",
+      //     "6. marketClose": "20:00",
+      //     "7. timezone": "UTC+02",
+      //     "8. currency": "EUR",
+      //     "9. matchScore": "0.7500",
+      //   },
+      //   {
+      //     "1. symbol": "BSTAX",
+      //     "2. name": "BlackRock Total Factor Fund Ivestor A shares",
+      //     "3. type": "Mutual Fund",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.7500",
+      //   },
+      //   {
+      //     "1. symbol": "BSTBF",
+      //     "2. name": "Betsson AB",
+      //     "3. type": "Equity",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.7500",
+      //   },
+      //   {
+      //     "1. symbol": "BSTCX",
+      //     "2. name": "BlackRock Total Factor Fund Investor C Shares",
+      //     "3. type": "Mutual Fund",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.7500",
+      //   },
+      //   {
+      //     "1. symbol": "BSTGD",
+      //     "2. name": "Biostage Inc",
+      //     "3. type": "Equity",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.7500",
+      //   },
+      //   {
+      //     "1. symbol": "BSTGF",
+      //     "2. name": "Boustead Singapore Ltd",
+      //     "3. type": "Equity",
+      //     "4. region": "United States",
+      //     "5. marketOpen": "09:30",
+      //     "6. marketClose": "16:00",
+      //     "7. timezone": "UTC-04",
+      //     "8. currency": "USD",
+      //     "9. matchScore": "0.7500",
+      //   },
+      //   {
+      //     "1. symbol": "BST.DEX",
+      //     "2. name": "Bastei Lübbe AG",
+      //     "3. type": "Equity",
+      //     "4. region": "XETRA",
+      //     "5. marketOpen": "08:00",
+      //     "6. marketClose": "20:00",
+      //     "7. timezone": "UTC+02",
+      //     "8. currency": "EUR",
+      //     "9. matchScore": "0.6667",
+      //   },
+      // ];
+      // const temp = [];
+      // for (let i = 0; i < res.length; i++) {
       //   temp.push({
-      //     name: response.data.bestMatches[i]["2. name"],
-      //     symbol: response.data.bestMatches[i]["1. symbol"],
+      //     name: res[i]["2. name"],
+      //     symbol: res[i]["1. symbol"],
       //   });
       // }
+
+
+
+      let temp = [];
+      for (let i = 0; i < response.data.bestMatches.length; i++) {
+        temp.push({
+          name: response.data.bestMatches[i]["2. name"],
+          symbol: response.data.bestMatches[i]["1. symbol"],
+        });
+      }
       setOptions(temp);
     } catch (error) {
       setLoading(false);
